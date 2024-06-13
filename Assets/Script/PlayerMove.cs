804056391b2bba172c04c2863b9bd415fb6ba81d
조준후 <jogusals4601@naver.com>
2024-06-13 15:31:48 +0900

    [Update] Character Animaition 및 Weapon Asset 추가

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -5,16 +5,17 @@ using UnityEngine.AI;
 
 public class PlayerMove : MonoBehaviour
 {
-    [Header("Componets")]
-    //[SerializeField] private NavMeshAgent NavAgent_Player;
+    [Header("Components")]
+    [SerializeField] private NavMeshAgent NavAgent_Player;
     [SerializeField] private Animator Animator_Player;
-    [SerializeField] private TextMesh TextMesh_HealthBar; // HP는 슬라이더로 대체 될 예정
+    [SerializeField] private TextMesh TextMesh_HealthBar;
     [SerializeField] private TextMesh Nick_Name;
     [SerializeField] private Transform Transform_Player;
 
     [Header("Movement")]
     [SerializeField] private float _ratationSpeed = 100.0f;
     [SerializeField] private float _moveSpeed = 5.0f;
+
 
     //[Header("Attack")]
     //[SerializeField] private KeyCode _atkKey = KeyCode.A;
@@ -23,12 +24,11 @@ public class PlayerMove : MonoBehaviour
 
     //[SerializeField] private int _health = 4;
 
-    public void Update()
+    private void Update()
     {
-        //SetHealthBarOnUpdate(_health);
-
         PlayerMoveOnUpdate();
     }
+
 
     //private void SetHealthBarOnUpdate(int health)
     //{
@@ -37,29 +37,25 @@ public class PlayerMove : MonoBehaviour
 
     private void PlayerMoveOnUpdate()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit hit, 100))
+        if (Input.GetMouseButtonDown(0)) // 좌클릭 시
         {
-            Debug.DrawLine(ray.origin, hit.point);
-            Vector3 lookRotate = new Vector3(hit.point.x, Transform_Player.position.y, hit.point.z);
-            Transform_Player.LookAt(lookRotate);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit, 100))
+            {
+                Debug.DrawLine(ray.origin, hit.point);
+                NavAgent_Player.SetDestination(hit.point); // 클릭한 위치로 이동
+            }
+        }
 
-            float horizontal = Input.GetAxis("Horizontal");
-            transform.Rotate(0, horizontal * _ratationSpeed * Time.deltaTime, 0);
-
-            float vetrial = Input.GetAxis("Vertical");
-            transform.Translate(Vector3.forward * vetrial * _moveSpeed * Time.deltaTime);
-
-
-            //if (Input.GetKeyDown(_atkKey))
-            //{
-            //    //CommandAtk();
-            //}
+        // 애니메이션이나 다른 동작을 추가할 수 있습니다.
+        // 예를 들어, NavMeshAgent가 이동 중인지 확인하고 애니메이션 상태를 업데이트합니다.
+        if (NavAgent_Player.remainingDistance > NavAgent_Player.stoppingDistance)
+        {
+            Animator_Player.SetBool("isMove", true);
+        }
+        else
+        {
+            Animator_Player.SetBool("isMove", false);
         }
     }
-
-    //private void CommandAtk()
-    //{
-    //    GameObject atkObjectForSpawn = Instantiate(Prefab_AtkObject, Tranform_AtkSpawnPos.transform.position, Tranform_AtkSpawnPos.transform.rotation);
-    //}
 }
