b81dc16bdf5e6c6c6af31e6ee96de8313b76a9ba
조준후 <jogusals4601@naver.com>
2024-06-19 00:56:38 +0900

    [Update]Skill_D(각성) 추가

--- Assets/Script/SkillManager.cs
+++ Assets/Script/SkillManager.cs
@@ -26,6 +26,13 @@ public class SkillManager : MonoBehaviour
     [Header("Skill_R")]
     [SerializeField] private ParticleSystem battle_Mode_Change_Effect;
     [SerializeField] private GameObject battle_Mode_Trigger;
+    private Animator battle_Mode_Animator;
+    [SerializeField] private RuntimeAnimatorController battle_Mode;
+    [SerializeField] private GameObject battle_Mode_Weapon_Start;
+    [SerializeField] private GameObject battle_Mode_Weapon_Saya;
+
+    [SerializeField] private GameObject battle_Mode_Weapon_Attack;
+    
 
     [Header("Common")]
     [SerializeField] private AudioSource shoot_Sound;
@@ -33,15 +40,37 @@ public class SkillManager : MonoBehaviour
     [SerializeField] private Transform attack_Pos;
     [SerializeField] private Transform effect_Pos;
     private PlayerMove playerMove;
+    private bool SetbattleMode = false;
+
 
 
     private void Start()
     {
         playerMove = GetComponent<PlayerMove>();
         sword_Attack_SoundClip = sword_Attack_Sound.clip;
+        battle_Mode_Animator = GetComponent<Animator>();
     }
 
     private void Update()
+    {
+        BattleStyle();
+    }
+
+    private void BattleStyle()
+    {
+        if (!SetbattleMode)
+        {
+            BaseBattleModeSkill();
+        }
+        else if (SetbattleMode)
+        {
+            AwakeBattleModeSkill();
+        }
+
+    }
+
+
+    private void BaseBattleModeSkill()
     {
         //스킬 Q(라이플스킬)
         if (Input.GetKeyDown(KeyCode.Q))
@@ -68,16 +97,23 @@ public class SkillManager : MonoBehaviour
         }
     }
 
-    IEnumerator OnDarknessShoot()
+    #region BaseBattleMode
+    IEnumerator OnDarknessShoot() // 스킬 Q
     {
+        playerMove.enabled = false;
         animator.SetTrigger("isDarkness");
         yield return new WaitForSeconds(0.8f);
+        changer.temp_SoundClip = shoot_Sound.clip;
+        shoot_Sound.clip = changer.darkness_Shoot_SoundClip;
+        shoot_Sound.PlayOneShot(shoot_Sound.clip);
         GameObject darknessShootBulletEffect = Instantiate(darkness_Shoot_Effect, effect_Pos.transform.position, effect_Pos.transform.rotation);
         Destroy(darknessShootBulletEffect, 0.5f);
 
         GameObject darknessShootBullet = Instantiate(darkness_Shoot_Bullet, attack_Pos.transform.position, attack_Pos.transform.rotation);
         Destroy(darknessShootBullet,2.0f);
+        shoot_Sound.clip = changer.temp_SoundClip;
         yield return null;
+        playerMove.enabled = true;
     }
 
 
@@ -96,6 +132,7 @@ public class SkillManager : MonoBehaviour
         shoot_Sound.PlayOneShot(shoot_Sound.clip);
         GameObject sniping_Effect = Instantiate(sniping_shoot_Effect, effect_Pos.transform.position, effect_Pos.transform.rotation);
         Destroy(sniping_Effect,0.5f);
+
         GameObject sniping_Bullet = Instantiate(sniping_shoot_Bullet, attack_Pos.transform.position, attack_Pos.transform.rotation);
         Destroy(sniping_Bullet, 2.0f);
         yield return new WaitForSeconds(0.8f);
@@ -149,9 +186,56 @@ public class SkillManager : MonoBehaviour
         yield return new WaitForSeconds(4.0f);
 
         battle_Mode_Trigger.gameObject.SetActive(false);
-        rifle.gameObject.SetActive(true);
-        katana_Sub.gameObject.SetActive(true);
+        battle_Mode_Animator.runtimeAnimatorController = battle_Mode;
+
+        battle_Mode_Weapon_Start.SetActive(true);
+        battle_Mode_Weapon_Saya.SetActive(true);
+        //rifle.gameObject.SetActive(true);
+        //katana_Sub.gameObject.SetActive(true);
+
         yield return null;
+        SetbattleMode = true;
         playerMove.enabled = true;
     }
+    #endregion
+
+
+    private void AwakeBattleModeSkill()
+    {
+        //스킬 Q(검 스킬)
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            StartCoroutine(KatanaSkillA());
+        }
+
+        ////스킬 W(발도)
+        //if (Input.GetKeyDown(KeyCode.W))
+        //{
+        //    
+        //}
+        //
+        ////스킬 E(대쉬)
+        //if (Input.GetKeyDown(KeyCode.E))
+        //{
+        //    
+        //}
+        //
+        ////스킬 R(각성기)
+        //if (Input.GetKeyDown(KeyCode.R))
+        //{
+        //   
+        //}
+    }
+
+    IEnumerator KatanaSkillA()
+    {
+        animator.SetTrigger("isAttack");
+        yield return new WaitForSeconds(0.6f);
+        battle_Mode_Weapon_Start.SetActive(false);
+        battle_Mode_Weapon_Attack.SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        battle_Mode_Weapon_Start.SetActive(true);
+        battle_Mode_Weapon_Attack.SetActive(false);
+    }
+
 }
