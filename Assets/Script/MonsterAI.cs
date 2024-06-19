2ef82030ef13cd8b0a2e0dda17df6233d8847c7e
조준후 <jogusals4601@naver.com>
2024-06-19 17:02:23 +0900

    [Update]Turret Object 수정

--- Assets/Script/MonsterAI.cs
+++ Assets/Script/MonsterAI.cs
@@ -5,9 +5,10 @@ public class MonsterAI : MonoBehaviour
 {
     [SerializeField] private int monsterType;// 몬스터 타입별로 근,원거리 공격하게 함
     [SerializeField] private Transform[] waypoints; 
-    [SerializeField] private Transform target;
+    public Transform target;
     [SerializeField] private float targetDetectionRange = 10f;
-    public float stoppingDistance = 1f; // 타겟에 근접하는 거리
+    public float stoppingDistance = 1.5f; // 타겟에 근접하는 거리
+    public float stoppingLongDistance = 5.0f; // 타겟에 근접하는 거리
     private Animator monsterAnimator;
 
     private NavMeshAgent agent;
@@ -34,10 +35,20 @@ public class MonsterAI : MonoBehaviour
             // 타겟이 일정 범위 내에 있을 때 타겟 근처로 이동
             Vector3 directionToTarget = (target.position - transform.position).normalized;
             Vector3 targetPosition = target.position - directionToTarget * stoppingDistance;
+            Vector3 targetLongDistancePosition = target.position - directionToTarget * stoppingLongDistance;
             agent.SetDestination(targetPosition);
-            if (gameObject.transform.position == targetPosition)
+            if (gameObject.transform.position == targetPosition && monsterType == 1)
             {
                 monsterAnimator.SetTrigger("isMonsterAttack");
+            }
+            else if (gameObject.transform.position == targetLongDistancePosition && monsterType == 2)
+            {
+                monsterAnimator.SetTrigger("isLongDistanceMonstgerAttack");
+            }
+            else if( gameObject.transform.position == targetLongDistancePosition && gameObject.CompareTag("Turret"))
+            { 
+                TurretRotate turretRotate = GetComponent<TurretRotate>();
+                turretRotate.rotateSpeed = 0;
             }
         }
         else
@@ -53,9 +64,13 @@ public class MonsterAI : MonoBehaviour
     void MoveToNextWaypoint()
     {
         if (waypoints.Length == 0)
+        {
             return;
+        }
 
         agent.SetDestination(waypoints[currentWaypointIndex].position);
         currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
     }
+
+    
 }
--- Assets/Script/TurretRotate.cs
+++ Assets/Script/TurretRotate.cs
@@ -2,11 +2,34 @@ using UnityEngine;
 
 public class TurretRotate : MonoBehaviour
 {
-    [SerializeField] private GameObject TurretHead;
-    [SerializeField] private float rotateSpeed = 5;
+
+    public GameObject TurretHead;
+    public float rotateSpeed = 100;
+    private MonsterAI monsterAI;
+
+    private void Start()
+    {
+        monsterAI = GetComponent<MonsterAI>();
+    }
 
     private void Update()
     {
-        TurretHead.transform.Rotate(Vector3.down * rotateSpeed *  Time.deltaTime) ;
+        Rotate();
+    }
+
+    private void Rotate()
+    {
+      TurretHead.transform.Rotate(Vector3.down * rotateSpeed * Time.deltaTime);
+        if (rotateSpeed == 0)
+        {
+            LookAtTarget();
+        }
+    }
+
+    void LookAtTarget()
+    {
+        Vector3 direction = (monsterAI.target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
     }
 }
