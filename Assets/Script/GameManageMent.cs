3c25963282886c32235b7d145f03316c746d7cfc
조준후 <jogusals4601@naver.com>
2024-06-21 02:46:02 +0900

    [Update] PlayerInfo Component 추가

--- Assets/Script/GameManageMent.cs
+++ Assets/Script/GameManageMent.cs
@@ -1,9 +1,7 @@
 using Cinemachine;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Audio;
-using UnityEngine.SceneManagement;
 
 public class GameManageMent : MonoBehaviour
 {
@@ -41,6 +39,17 @@ public class GameManageMent : MonoBehaviour
     [Header("Character_Create")]
     [SerializeField] private CinemachineVirtualCamera virtual_Camera;
     [SerializeField] private GameObject Player_Prefap;
+    [SerializeField] private PlayerInfo player_Info;
+    [SerializeField] private Slider player_Hp;
+    [SerializeField] private Slider player_Ep;
+
+
+    [SerializeField] private Text playerNickName_Text;
+    [SerializeField] private Text playerLevel_Text;
+    [SerializeField] private Text playerHp_Text;
+    [SerializeField] private Text playerEp_Text;
+    [SerializeField] private Text playerAp_Text;
+    [SerializeField] private Text playerDp_Text;
 
 
     [Header("MiniMap")]
@@ -84,6 +93,7 @@ public class GameManageMent : MonoBehaviour
         GameObject Player = Instantiate(Player_Prefap);
         virtual_Camera.Follow = Player.transform;
         miniMap.player = Player.transform;
+        player_Info = Player.GetComponent<PlayerInfo>();
     }
 
     private void Update()
@@ -92,6 +102,17 @@ public class GameManageMent : MonoBehaviour
         {
             Debug.Log("메뉴창 오픈하기");
         }
+
+        playerNickName_Text.text = $"{player_Info.playerNickName}";
+        playerLevel_Text.text = $"Lv. {player_Info.playerLevel}";
+        playerHp_Text.text = $"HP : {player_Info.playerNowHp} / {player_Info.playerMaxHp}";
+        playerEp_Text.text = $"HP : {player_Info.playerNowEp} / {player_Info.playerMaxEp}";
+        playerAp_Text.text = $"AP : {player_Info.playerAp}";
+        playerDp_Text.text = $"DP : {player_Info.playerDp}";
+
+        player_Hp.value = player_Info.playerNowHp / player_Info.playerMaxHp;
+        player_Ep.value = player_Info.playerNowEp / player_Info.playerMaxEp;
+
     }
 
     private void ChangeCursor(Texture2D cursorTexture, Vector2 hotSpot)
---
+++ Assets/Script/PlayerInfo.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerInfo : MonoBehaviour
+{
+    public string playerNickName = "³ªÃ÷";
+    public int playerLevel = 1;
+    public float playerNowHp = 500;
+    public float playerMaxHp = 500;
+    public float playerNowEp = 300;
+    public float playerMaxEp = 300;
+    public float playerAp = 50;
+    public float playerDp = 30;
+}
